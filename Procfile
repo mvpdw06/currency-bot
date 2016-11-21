454d3669b50b08f6e578811e23ee15849b2dd28c
Ryan Hsu <mvpdw06@gmail.com>
2016-11-21 20:18:36 +0800

    change test socket.io

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node testTelegramBot.js
+web: node testTelegramBot.js 
--- testTelegramBot.js
+++ testTelegramBot.js
@@ -4,6 +4,10 @@ var token = process.env.token;
 
 console.log('token', token);
 
+var port = process.env.PORT || 8080;
+
+var io = require('socket.io').listen(app.listen(port));
+
 var bot = new TelegramBot(token, { polling: true });
 
 console.log('bot', bot);
