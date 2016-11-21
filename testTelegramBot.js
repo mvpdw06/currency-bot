f6f7f945470c8cd6deaab2734d43ec8580648973
Ryan Hsu <mvpdw06@gmail.com>
2016-11-21 20:21:24 +0800

    change test

--- testTelegramBot.js
+++ testTelegramBot.js
@@ -7,7 +7,7 @@ console.log('token', token);
 var port = process.env.PORT || 8443;
 var host = process.env.HOST;
 
-var bot = new TelegramBot(token, {webHook: {port: port, host: host});
+var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 
 console.log('bot', bot);
 
