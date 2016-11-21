12d43d943f5693210b426f72e10f586953e9e239
Ryan Hsu <mvpdw06@gmail.com>
2016-11-21 23:43:00 +0800

    change test sendMessage

--- testBot.js
+++ testBot.js
@@ -2,7 +2,7 @@ var TelegramBot = require('node-telegram-bot-api');
 
 // get from Heroku config variable
 var token = process.env.token;
-// var channelID = +(process.env.channelID);
+var channelID = +(process.env.channelID);
 var port = process.env.PORT || 8443;
 var host = process.env.HOST;
 
@@ -20,6 +20,6 @@ var bot = new TelegramBot(token, webhook);
 console.log('new bot');
 
 // var interval = 3 * 3600 * 1000;
-bot.getMe().then(function(response){
+bot.sendMessage(channelID, 'test Bot: Hello~~~~').then(function(response){
 	console.log('res:', response);
 });
