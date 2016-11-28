def73f2966862cc2791bb6c77a10d560037c7779
RyanHsu <mvpdw06@gmail.com>
2016-11-28 15:15:58 +0800

    fix interval schdule bug.

--- sendMessage.js
+++ sendMessage.js
@@ -18,7 +18,7 @@ const webhook = {
 const bot = new TelegramBot(token, webhook);
 later.date.timezone("Asia/Taipei");
 // const sched = later.parse.recur().on(8, 9, 10, 11, 12, 13, 14, 15, 16, 17).hour().onWeekday();
-const sched = later.parse.recur().every(0).minute().after('08:00').time().before('16:00').time();
+const sched = later.parse.recur().on(0).minute().after('08:00').time().before('16:00').time();
 // const sched = later.parse.recur().onWeekend().on(10).second();
 
 console.log('app start!');
